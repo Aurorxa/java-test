212792f1c6fe495eeda93ade37f879723090a5d9
许大仙 <1900919313@qq.com>
2025-05-23 15:05:54 +0800

    feat: 添加 JMH 性能测试
    - 新增 SumTest 类，包含 4 种不同方式的求和方法
    - 添加 JMH 依赖并配置 Maven 项目
    - 创建简单的 web 项目结构，包括 web.xml 和 index.jsp
    - 添加 .gitignore 文件，忽略常见 IDE 和系统文件

---
+++ src/test/java/com/github/ExecutorTest.java
@@ -0,0 +1,35 @@
+package com.github;
+
+import com.github.domain.CsvReader;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDateTime;
+import java.time.YearMonth;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+@Slf4j
+public class ExecutorTest {
+
+    public static void monthAnalysis() throws Exception {
+        Map<String, Long> map = CsvReader.readCsvFile().collect(Collectors.groupingBy(
+                (csv) -> {
+                    LocalDateTime eventTime = csv.getEventTime();
+                    return YearMonth
+                            .from(eventTime)
+                            .toString();
+                },
+                TreeMap::new,
+                Collectors.counting()));
+
+        map.forEach((key, value) -> System.out.println(key + " 的订单数是：" + value));
+    }
+
+    public static void main(String[] args) throws Exception {
+        log.info("开始统计");
+        monthAnalysis();
+        log.info("执行其它操作");
+    }
+
+}
