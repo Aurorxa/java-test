54386aca54596ecab45513e29253e29f5f504d70
许大仙 <1900919313@qq.com>
2025-05-23 14:42:04 +0800

    feat: 添加 JMH 性能测试
    - 新增 SumTest 类，包含 4 种不同方式的求和方法
    - 添加 JMH 依赖并配置 Maven 项目
    - 创建简单的 web 项目结构，包括 web.xml 和 index.jsp
    - 添加 .gitignore 文件，忽略常见 IDE 和系统文件

--- src/test/java/com/github/LambdaTest.java
+++ src/test/java/com/github/LambdaTest.java
@@ -111,19 +111,101 @@ public class LambdaTest {
                 Collectors.groupingBy(CsvRecord::getUserId, Collectors.counting())));
 
         // 对每个地区用户的订单数求最大值
-        Map<String, Optional<Map.Entry<String, Long>>> me = map
+        Map<String, Optional<Map.Entry<String, Long>>> resultMap = map
                 .entrySet()
                 .stream()
-                .collect(Collectors.toMap(Map.Entry::getKey, entry -> entry
-                        .getValue()
-                        .entrySet()
-                        .stream()
-                        .max(Map.Entry.comparingByValue())));
+                .map(
+                        e -> Map.entry(e.getKey(),
+                                e
+                                        .getValue()
+                                        .entrySet()
+                                        .stream()
+                                        .max(Map.Entry.comparingByValue()))
+                )
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
         // 遍历 Map
-        me.forEach((area, entry) -> {
+        resultMap.forEach((area, entry) -> {
             Map.Entry<String, Long> m = entry.orElse(new AbstractMap.SimpleEntry<>("", 0L));
             System.out.printf("地区：%s，下单最多的用户id：%s，订单数是：%s \n", area, m.getKey(), m.getValue());
         });
+    }
+
+    @Test
+    public void testAnalyzeMaxOrderByAreaTop3() {
+        // 根据地区分组，再根据用户分组，获取每个地区每个用户的订单数
+        Map<String, Map<String, Long>> map = stream.collect(Collectors.groupingBy(
+                CsvRecord::getLocal,
+                Collectors.groupingBy(CsvRecord::getUserId, Collectors.counting())));
+
+        // 对每个地区用户的订单数求前 3 名
+        Map<String, List<Map.Entry<String, Long>>> resultMap = map
+                .entrySet()
+                .stream()
+                .map(
+                        e -> Map.entry(e.getKey(),
+                                e
+                                        .getValue()
+                                        .entrySet()
+                                        .stream()
+                                        .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                                        .limit(3)
+                                        .collect(Collectors.toList()))
+                )
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        // 遍历 Map
+        resultMap.forEach((area, list) -> {
+            System.out.println("地区：" + area);
+            System.out.println("下单最多的前 3 名信息：");
+            list.forEach((e) -> {
+                final String key = e.getKey();
+                final Long value = e.getValue();
+                System.out.println("用户id：" + key + "，订单数：" + value);
+            });
+
+            System.out.println("--------------------------------------------");
+        });
+    }
+
+    @Test
+    public void testAnalyzeOrderByCategoryCode() {
+        Map<String, Long> map = stream
+                .filter(csv -> !csv
+                        .getCategoryCode()
+                        .isEmpty())
+                .collect(Collectors.groupingBy(
+                        (csv)-> {
+                            final String categoryCode = csv
+                                    .getCategoryCode();
+                            final int index = categoryCode
+                                    .indexOf(".");
+                            return categoryCode.substring(0, index);
+                        },
+                        TreeMap::new,
+                        Collectors.counting()));
+
+        map.forEach((key, value) -> System.out.println(key + " 的订单数是：" + value));
+    }
+
+    @Test
+    public void testAnalyzeInterval() {
+
+        Map<String, Long> map = stream
+                .filter(csv->csv.getPrice() > 0)
+                .collect(Collectors.groupingBy(
+                        (csv)->getPriceRangeLabel(csv.getPrice()),
+                        TreeMap::new,
+                        Collectors.counting()));
+
+        map.forEach((key, value) -> System.out.println(key + " 的订单数是：" + value));
+    }
+
+    private static String getPriceRangeLabel(double price) {
+        if (price < 100) return "[0,100)";
+        else if (price < 500) return "[100,500)";
+        else if (price < 1000) return "[500,1000)";
+        else return "[1000,+∞)";
     }
 
 }
